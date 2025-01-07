1210424bbaf153a9e83b90ba3eae201127dd2bc5
Diego Neira <diegoneira@gmail.com>
2025-01-06 23:24:30 -0300

    debugg misspelling index.html and tutorial part 3

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,21 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
-# Create your views here.
-
 def index(request):
-    lastest_question_list = Question.objects.order_by("-pub_date")[:2]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "lastest_question_list": lastest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    lastest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"lastest_question_list": lastest_question_list}
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
@@ -25,3 +21,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
