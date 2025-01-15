eec7fab35e2d24cecbb8b1e95da1c94a909e5fcd
Diego Neira <diegoneira@gmail.com>
2025-01-15 18:17:03 -0300

    tutorial 3.7

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     # ej /polls/
     path("", views.index, name="index"),
     # ej /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:pk>/", views.DetailViews.as_view(), name="detail"),
     # ej /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     # ej /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
